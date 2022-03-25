c17d41f0bf3d6cc173f5818f1a2778c34b9ab022
mrkleiner <megaadrenaline1055@gmail.com>
2022-03-25 08:14:26 +0200

    proper brush texture alignment
    cross product stuff. Finally, proper texture alignment

--- mods/vmf_export/brush_ents.py
+++ mods/vmf_export/brush_ents.py
@@ -47,24 +47,24 @@ def blfoil_get_island_facepair(ob):
 	mesh = obj.data
 	paths={v.index:set() for v in mesh.vertices}
 	for e in mesh.edges:
-	    paths[e.vertices[0]].add(e.vertices[1])
-	    paths[e.vertices[1]].add(e.vertices[0])
+		paths[e.vertices[0]].add(e.vertices[1])
+		paths[e.vertices[1]].add(e.vertices[0])
 	lparts=[]
 	while True:
-	    try:
-	        i=next(iter(paths.keys()))
-	    except StopIteration:
-	        break
-	    lpart={i}
-	    cur={i}
-	    while True:
-	        eligible={sc for sc in cur if sc in paths}
-	        if not eligible:
-	            break
-	        cur={ve for sc in eligible for ve in paths[sc]}
-	        lpart.update(cur)
-	        for key in eligible: paths.pop(key)
-	    lparts.append(lpart)
+		try:
+			i=next(iter(paths.keys()))
+		except StopIteration:
+			break
+		lpart={i}
+		cur={i}
+		while True:
+			eligible={sc for sc in cur if sc in paths}
+			if not eligible:
+				break
+			cur={ve for sc in eligible for ve in paths[sc]}
+			lpart.update(cur)
+			for key in eligible: paths.pop(key)
+		lparts.append(lpart)
 
 	print(lparts)
 
@@ -73,19 +73,19 @@ def blfoil_get_island_facepair(ob):
 	isle_pairs = []
 
 	for num, super_isle in enumerate(lparts):
-	    match_faces = []
-	    for nad in ob.data.polygons:
-	        for hah in nad.vertices:
-	            if hah in super_isle:
-	                match_faces.append(nad)
-	    matched_faces_to = list(dict.fromkeys(match_faces))
-	    # isle_pairs[num] = matched_faces_to
-	    isle_pairs.append(matched_faces_to)
-	    print(num, 'matched faces:', matched_faces_to)
-	    
-	    for vev in matched_faces_to:
-	        for rer_vert in vev.vertices:
-	            print(rer_vert)
+		match_faces = []
+		for nad in ob.data.polygons:
+			for hah in nad.vertices:
+				if hah in super_isle:
+					match_faces.append(nad)
+		matched_faces_to = list(dict.fromkeys(match_faces))
+		# isle_pairs[num] = matched_faces_to
+		isle_pairs.append(matched_faces_to)
+		print(num, 'matched faces:', matched_faces_to)
+		
+		for vev in matched_faces_to:
+			for rer_vert in vev.vertices:
+				print(rer_vert)
 
 	return isle_pairs
 
@@ -94,12 +94,28 @@ def blfoil_get_island_facepair(ob):
 
 	print('br')
 	for solid in isle_pairs:
-	    print('island number:', solid)
-	    print(isle_pairs[solid])
-    print(isle_pairs[0][1].index)
+		print('island number:', solid)
+		print(isle_pairs[solid])
+	print(isle_pairs[0][1].index)
 	"""
 
-
+def rotation_matrix(axis, theta):
+	"""
+	Return the rotation matrix associated with counterclockwise rotation about
+	the given axis by theta radians.
+	"""
+	import numpy as np
+	import math
+	
+	axis = np.asarray(axis)
+	axis = axis / math.sqrt(np.dot(axis, axis))
+	a = math.cos(theta / 2.0)
+	b, c, d = -axis * math.sin(theta / 2.0)
+	aa, bb, cc, dd = a * a, b * b, c * c, d * d
+	bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+	return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+					[2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+					[2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
 
 
 
@@ -108,11 +124,14 @@ def vert_uv_math(tverts):
 	Expects a tuple of three vert location vectors: (Vector, Vector, Vector)
 	IMPORTANT: VERT LOCATIONS HAVE TO BE WORLD SPACE!!!!!!
 	"""
-
+	import numpy as np
 	import math
+
+	
 
 	cuvc = tverts
 
+	# calc U
 	uwidth_a = cuvc[2][0] - cuvc[1][0]
 	uwidth_b = cuvc[2][1] - cuvc[1][1]
 	uwidth_c = cuvc[2][2] - cuvc[1][2]
@@ -135,9 +154,32 @@ def vert_uv_math(tverts):
 	calc_vy = vwidth_b / calc_vwidth
 	calc_vz = vwidth_c / calc_vwidth
 
+
+	#
+	# Actually, this is what hammer does too (kinda)
+	#
+
+	# three verts form two vectors.
+	# if those verts are not from a perfect square - the angle between them is not 90 degrees
+	vec_a = (calc_ux, calc_uy, calc_uz)
+	vec_b = (calc_vx, calc_vy, calc_vz)
+
+	# get cross product of those vectors
+	vec_c = np.cross(vec_a, vec_b)
+
+	# prepare to rotate things by 90 degrees in radians
+	radians_angle = math.radians(90)
+
+	# first vector remains unchanged
+	result_a = vec_a
+
+	# now, use original vector A as an axis and rotate cross product around it
+	result_b = np.dot(rotation_matrix(vec_a, radians_angle), vec_c)
+
+
 	rer = {
-		'u': (calc_ux, calc_uy, calc_uz),
-		'v': (calc_vx, calc_vy, calc_vz)
+		'u': result_a,
+		'v': result_b
 	}
 
 	return rer
@@ -244,7 +286,8 @@ def blfoil_easy_brushes(objct):
 		isl_payload = []
 		for isle_face in isle:
 
-			# reversed
+			# todo: when exactly the ensure_lookup should happen?
+			# could it happen on per-island basis?
 			bm.faces.ensure_lookup_table()
 			bm.verts.ensure_lookup_table()
 			revo_energy = [obj_mt_world @ obj_verts[revo].co for revo in reversed(isle_face.vertices)]
@@ -258,11 +301,8 @@ def blfoil_easy_brushes(objct):
 			# but like... it's just one if, why bother with maths ?
 			uv_verts = (three_v[0], three_v[1], three_v[2])
 			if len(revo_energy) > 4:
-				# lst = list(range(165))
-				# lst[0::10]
 				vtxstep = math.floor(len(revo_energy) / 3)
 				uv_verts = revo_energy[0::vtxstep]
-				# uv_verts = (revo_energy[0], revo_energy[(math.ceil(len(revo_energy) / 2))], revo_energy[-1])
 
 			# important todo: wait, what's the point of including 3 verts separately?
 			face_payload = {
@@ -273,11 +313,13 @@ def blfoil_easy_brushes(objct):
 			}
 
 			isl_payload.append(face_payload)
+
 		print('island took', int(round(time.time() * 1000)) - isl_take)
 
 		easy_islands.append(isl_payload)
 
 
 	bm.free()
-	print('Done with islands')
+	print('Done with islands of the object', objct.name)
 	return easy_islands
+
--- mods/vmf_export/do_export.py
+++ mods/vmf_export/do_export.py
@@ -13,18 +13,10 @@ def blfoil_vmf_exporter(self, context, entity_definition, tgt_vmf):
     import time
 
     print('exec export')
-    print(eval_state(1))
+
     vp_blpe_ents = entity_definition
 
-    
-    # cdata_cleanup(self, context)
-    
-
-    # for testing purposes - create vmf from scratch
     iguana = lizardvmf(tgt_vmf)
-    print(iguana.mapsettings['skyname'])
-    # iguana.mapsettings['skyname'] = 'fuck_you'
-    # print(iguana.tovmf())
 
     obj_applicable = []
 
@@ -32,16 +24,18 @@ def blfoil_vmf_exporter(self, context, entity_definition, tgt_vmf):
     # todo: it's actually smarter to do it like this
     # cbt_victim = [obj for obj in bpy.data.objects if len(obj.ent_conf.obj_ent_type) > 3 and obj.ent_conf.obj_ent_type != 'nil' and obj.ent_conf.obj_ent_type != 'light_spot']
     for apl in context.scene.objects:
-        if len(apl.ent_conf.obj_ent_type) > 3 and apl.ent_conf.obj_ent_type != 'nil' and apl.ent_conf.obj_ent_type != 'light_spot':
+        if len(apl.ent_conf.obj_ent_type) > 3 and not apl.ent_conf.obj_ent_type in ['nil', 'light_spot', 'light']:
             obj_applicable.append(apl)
-
-
-    # test
-    operands = []
 
     # if scene lacks id pool - create one
     if context.scene.get('blfoil_id_pool') == None:
         context.scene['blfoil_id_pool'] = []
+
+
+
+    # ========================================================
+    #          Write everything except brushes
+    # ========================================================
 
 
     # for every object which is a blfoil entity
@@ -49,11 +43,11 @@ def blfoil_vmf_exporter(self, context, entity_definition, tgt_vmf):
 
         cent_type = ob.ent_conf.obj_ent_type
 
-        # first - regular entities
         # entities one by one
         regular_ent = blfoil_to_ent(ob, vp_blpe_ents, iguana, self, context)
 
-        obj_mt_world = ob.matrix_world
+        # obj_mt_world = ob.matrix_world
+
 
 
         # =======================================
@@ -74,7 +68,8 @@ def blfoil_vmf_exporter(self, context, entity_definition, tgt_vmf):
             easy_b = blfoil_easy_brushes(ob)
 
 
-            # for every island (brush)
+            # get the total amount of the brush islands
+            # and multiply by 2 because id reassignment could happen later
             print('reserve brush ids')
             reserved_brush_ids = iguana.getfreeid((len(easy_b) * 2) + 1)
 
@@ -227,113 +222,6 @@ def blfoil_vmf_exporter(self, context, entity_definition, tgt_vmf):
 
     """
 
-
-    sce_vmf_path = str('E:\\!!Blend_Projects\\scripts\\entity_exporter\\ents.vmf')
-
-    file = open(sce_vmf_path)
-
-    # all possible ents
-    prop_ents = vp_blpe_ents
-    
-    
-    # we export spotlights separately
-    cbt_victim = [obj for obj in bpy.data.objects if len(obj.ent_conf.obj_ent_type) > 3 and obj.ent_conf.obj_ent_type != 'nil' and obj.ent_conf.obj_ent_type != 'light_spot']
-    
-    
-    
-    print(cbt_victim)
-    
-    # spotlights
-    monitor_lizard = [obj for obj in bpy.data.objects if len(obj.ent_conf.obj_ent_type) > 3 and obj.ent_conf.obj_ent_type != 'nil' and obj.ent_conf.obj_ent_type == 'light_spot' and obj.type == 'LIGHT' and obj.data.type == 'SPOT']
-    # monitor_lizard = [obj for obj in bpy.data.objects if len(obj.ent_conf.obj_ent_type) > 3 and obj.ent_conf.obj_ent_type != 'nil' and obj.ent_conf.obj_ent_type == 'light_spot']
-    
-    constructed_ents = []
-    
-    
-    # -----------
-    #   Write
-    # -----------
-    for cbt in cbt_victim:
-        mk_ent = []
-        
-        print('processing object: ' + str(cbt))
-        
-        cent_type = cbt.ent_conf.obj_ent_type
-        
-        # get transforms from blender
-        obj_locrot = get_obj_locrot_v1(cbt, 1, 'z', self, context)
-        
-        
-        #
-        # write shared
-        #
-        
-        # write opening
-        mk_ent.append('entity\n{\n')
-        mk_ent.append('\t' + '"classname" "' + cent_type + '"\n')
-        mk_ent.append('\t' + '"liz3" "1"\n')    
-        
-        # any entity should have an origin - write origin (loc)
-        mk_ent.append('\t' + '"origin" "' + str(obj_locrot['loc'][0]) + ' ' + str(obj_locrot['loc'][1]) + ' ' + str(obj_locrot['loc'][2]) + '"\n')
-        
-        # if it's stated in the blender config json block that this entity should have angles - write anlges
-        if int(vp_blpe_ents[cent_type][9]['angles_enabled']) == 1:
-            
-            mk_ent.append('\t' + '"angles" "' + str(obj_locrot['rot'][1]) + ' ' + str(obj_locrot['rot'][2]) + ' ' + str(obj_locrot['rot'][0]) + '"\n')
-        
-        
-        # write strings
-        for str_j_idx, str_pr in enumerate(vp_blpe_ents[cent_type][0]):
-            # strings are never empty, unless you take them off
-            if cbt.ent_conf['pr_str_' + str(str_j_idx + 1)] != ' ':
-                mk_ent.append('\t' + '"' + vp_blpe_ents[cent_type][0][str_pr].split(':-:')[0] + '" "' + cbt.ent_conf['pr_str_' + str(str_j_idx + 1)] + '"\n')
-
-
-        # write ints
-        for int_j_idx, int_pr in enumerate(vp_blpe_ents[cent_type][1]):
-            mk_ent.append('\t' + '"' + vp_blpe_ents[cent_type][1][int_pr].split(':-:')[0] + '" "' + str(cbt.ent_conf['pr_int_' + str(int_j_idx + 1)]) + '"\n')
-        
-        
-        # write floats
-        for float_j_idx, float_pr in enumerate(vp_blpe_ents[cent_type][2]):
-            mk_ent.append('\t' + '"' + vp_blpe_ents[cent_type][2][float_pr].split(':-:')[0] + '" "' + str(round(cbt.ent_conf['pr_float_' + str(float_j_idx + 1)], 4)) + '"\n')
-
-
-        # write colors
-        for color_j_idx, color_pr in enumerate(vp_blpe_ents[cent_type][3]):
-            rgb = str(int(cbt.ent_conf['pr_color_' + str(color_j_idx + 1)][0] * 255)) + ' ' + str(int(cbt.ent_conf['pr_color_' + str(color_j_idx + 1)][1] * 255)) + ' ' + str(int(cbt.ent_conf['pr_color_' + str(color_j_idx + 1)][2] * 255))
-            mk_ent.append('\t' + '"' + vp_blpe_ents[cent_type][3][color_pr].split(':-:')[0] + '" "' + rgb + '"\n')
-
-
-        # write enums
-        for enum_j_idx, enum_pr in enumerate(vp_blpe_ents[cent_type][4]):
-            mk_ent.append('\t' + '"' + cbt.ent_conf['ob_enum_tgt_' + str(enum_j_idx + 1)].split(':-:')[0] + '" "' + cbt.ent_conf['ob_enum_tgt_' + str(enum_j_idx + 1)].split(':-:')[1] + '"\n')
-            # print(cbt.ent_conf['pr_enum_1'])
-
-
-
-        # write enum booleans
-        for bool_enum_j_idx, bool_enum_pr in enumerate(vp_blpe_ents[cent_type][5]):
-            mk_ent.append('\t' + '"' + vp_blpe_ents[cent_type][5][bool_enum_pr].split(':-:')[0] + '" "' + str(int(cbt.ent_conf['pr_enum_bool_' + str(bool_enum_j_idx + 1)])) + '"\n')
-
-        # write sflags
-        # If there are no flags - don't write the spawnflags at all
-        if len(vp_blpe_ents[cent_type][6]) > 0:
-            mk_ent.append('\t' + '"spawnflags" "' + str(cbt.ent_conf['l3_ent_sflags']) + '"\n')
-
-        
-        # write closing        
-        mk_ent.append('}\n')
-
-        # write constructed ent
-        constructed_ents.append(''.join(mk_ent))
-    
-    
-    
-    
-    
-    
-    
     constructed_spotlights = []
     # write spotlights
     for lizard in monitor_lizard:
