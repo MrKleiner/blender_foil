eab5d753315cbe61bf8031dd87d110f0f9845f94
mrkleiner <megaadrenaline1055@gmail.com>
2022-05-07 16:42:26 +0300

    update keyvalues

--- utils/kv/kvs.py
+++ utils/kv/kvs.py
@@ -1,3 +1,4 @@
+"""
 why = [
     {
         'keyname': 'lizards',
@@ -66,6 +67,28 @@ printStructv(why)
 
 
 
+====================================== SOURCE
+ def printStruct(struc, indent=0):
+   if isinstance(struc, dict):
+     print ('  '*indent+'{')
+     for key,val in struc.items():
+       if isinstance(val, (dict, list, tuple)):
+         print ('  '*(indent+1) + str(key) + '=> ')
+         printStruct(val, indent+2)
+       else:
+         print ('  '*(indent+1) + str(key) + '=> ' + str(val))
+     print ('  '*indent+'}')
+   elif isinstance(struc, list):
+     print ('  '*indent + '[')
+     for item in struc:
+       printStruct(item, indent+1)
+     print ('  '*indent + ']')
+   elif isinstance(struc, tuple):
+     print ('  '*indent + '(')
+     for item in struc:
+       printStruct(item, indent+1)
+     print ('  '*indent + ')')
+   else: print ('  '*indent + str(struc))
 
 
 
@@ -74,6 +97,9 @@ printStructv(why)
 
 
 
+
+
+"""
 
 
 
@@ -146,6 +172,35 @@ def adds(wh):
     return wh
 
 def printStruct(struc, indent=0):
+	global result, kl
+	if isinstance(struc, dict):
+		#print ('  '*indent+'{')
+		for key,val in struc.items():
+			if isinstance(val, (dict, list, tuple)):
+				#print ('  '*(indent+1) + '"' + str(key) + '" ')
+				printStruct(val, indent+2)
+			else:
+				if key == 'keycontent' and isinstance(val, str):
+					result += adds (' '*(15 - kl) + ' "' + str(val) + '"')
+				if key == 'keyname':
+					kl = len(str(val))
+					result += drop ('  '*(indent+1) + '"' + str(val) + '"')
+					#print ('  '*indent+'}')
+	elif isinstance(struc, list):
+		result += drop ('  '*(indent-1) + '{')
+		for item in struc:
+		printStruct(item, indent+1)
+		result += drop ('  '*(indent-1) + '}')
+	elif isinstance(struc, tuple):
+		result += drop ('  '*indent + '(')
+	for item in struc:
+		printStruct(item, indent+1)
+		result += drop ('  '*indent + ')')
+	else: result += drop ('  '*indent + str(struc))
+
+
+"""
+def printStruct(struc, indent=0):
   global result, kl
   if isinstance(struc, dict):
     #print ('  '*indent+'{')
@@ -172,7 +227,7 @@ def printStruct(struc, indent=0):
     result += drop ('  '*indent + ')')
   else: result += drop ('  '*indent + str(struc))
     
-
+"""
 
 d = [{'a1':1, 'a2':2, 'a3':3}, [1,2,3], [{'b1':1, 'b2':2}, {'c1':1}], 'd1', 'd2', 'd3']
 
@@ -184,6 +239,3 @@ printStruct(why)
 print('result')
 
 print(result)
-
-
-
